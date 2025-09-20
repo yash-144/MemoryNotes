e9de4e2762aaf6745c3d16e58339b5b3109784e5
yash-144 <goyalyash144@gmail.com>
2025-09-20 02:13:43 -0700

    added changes saved/unsaved indicator. added number of characters indicator. and minor UI update

--- app/src/main/java/com/pixbuilds/memorynotes/MainActivity.kt
+++ app/src/main/java/com/pixbuilds/memorynotes/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
